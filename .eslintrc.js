59a370043bc75006a8c270b389a4fa98d0cb5f10
Tiago Boeing <contato@tiagoboeing.com>
2022-12-20 16:14:05 -0300

    build: :rotating_light: Recreated ESLint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,23 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
   env: {
-    es6: true,
-    node: true,
-    jest: true
+    browser: true,
+    es2021: true,
+    node: true
   },
-  plugins: [
-    "@typescript-eslint"
-  ],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "standard"
-  ],
-  globals: {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
-  },
+  extends: 'standard-with-typescript',
+  overrides: [],
   parserOptions: {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
   },
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "no-use-before-define": "off",
-    "space-before-function-paren": [
-      "error",
-      "always"
-    ],
-    "no-useless-constructor": 0,
-    "camelcase": 0
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'no-use-before-define': 'off',
+    'space-before-function-paren': ['error', 'always'],
+    'no-useless-constructor': 0,
+    camelcase: 0
   }
 }
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,7 @@
-describe('Index', () => { 
+import { describe } from '@jest/globals'
+
+describe('Index', () => {
   it('should be true', () => {
     expect(true).toBeTruthy()
-  });
- })
+  })
+})
